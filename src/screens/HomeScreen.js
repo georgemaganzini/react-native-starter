d1dd88d04dbbdc7875d87b1c2a4fffbbf059bc59
georgemaganzini <involver7@gmail.com>
2022-03-14 14:21:51 -0700

    Section 6 complete

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -24,7 +24,9 @@ const HomeScreen = ({navigation}) => {
       <Button 
       title="Go to Square Demo"
       onPress={()=>navigation.navigate('Square')}/>
-     
+      <Button 
+      title="Go to Text Demo"
+      onPress={()=>navigation.navigate('Text')}/>
     </View>
   )
 };
