ba63ea8ea20836595f06684fbc4adad8400fae71
georgemaganzini <involver7@gmail.com>
2022-03-14 11:16:11 -0700

    Add randomly colored view generator as a FlatList

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -15,6 +15,12 @@ const HomeScreen = ({navigation}) => {
       <Button 
       title="Go to Image Demo"
       onPress={()=>navigation.navigate('Image')}/>
+      <Button 
+      title="Go to Counter Demo"
+      onPress={()=>navigation.navigate('Counter')}/>
+      <Button 
+      title="Go to Color Demo"
+      onPress={()=>navigation.navigate('Color')}/>
      
     </View>
   )
